d892891b736fee7096529101e4d1f8ba6ea808b3
Amir Rassaei <amirrassaei1376@gmail.com>
2021-08-01 14:41:27 +0200

    Fix header and feature section except (xl screen)

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const togglerBox = document.querySelector(".nav__toggler");
 const togglerIcon = document.querySelector(".nav__toggler--icon");
 
 // events
-togglerBox.addEventListener("click", function () {
+togglerBox.addEventListener("click", function (e) {
+  e.preventDefault();
   console.log("clicked!");
   togglerBox.classList.toggle("toggle");
 });
