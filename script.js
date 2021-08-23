7e8882bb91f583f81a0baa10ccb2f1e66bc59a2f
Amir Rassaei <amirrassaei1376@gmail.com>
2021-08-23 14:23:34 +0200

    ADD (_header.scss): dropdown menus

--- script.js
+++ script.js
@@ -5,6 +5,5 @@ const togglerIcon = document.querySelector(".nav__toggler--icon");
 // events
 togglerBox.addEventListener("click", function (e) {
   e.preventDefault();
-  console.log("clicked!");
   togglerBox.classList.toggle("toggle");
 });
