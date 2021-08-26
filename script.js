1c9566c0e86494d95ea830783fd5884e365fb1b2
Amir Rassaei <amirrassaei1376@gmail.com>
2021-08-26 16:57:19 +0200

    Add (_header.scss , script.js): navbar collapse However, code needs to be optimized especiallly in _header.scss

--- script.js
+++ script.js
@@ -1,8 +1,57 @@
+"use strict";
+
+// global variables
+let navIsActive = false;
+
 // selectors
-const togglerBox = document.querySelector(".navbar__toggler");
+const burger = document.querySelector(".navbar__toggler");
+const navigation = document.querySelector(".navbar__collapse");
+const navLinks = document.querySelector(".navbar__nav");
+const subMenus = document.querySelectorAll(".nav__sub-menu");
+const navArrows = document.querySelectorAll(".nav__link-arrow");
 
 // events
-togglerBox.addEventListener("click", function (e) {
-  e.preventDefault();
-  togglerBox.classList.toggle("toggle");
-});
+
+const activateNav = () => {
+  burger.addEventListener("click", function (e) {
+    e.preventDefault();
+    navIsActive = !navIsActive;
+    // toggler animation
+    burger.classList.toggle("toggler-active");
+
+    // open the nav
+    navigation.classList.toggle("nav-active");
+
+    // resetting menu style when it is closed
+    // close sub-menu when the menu was closed
+    if (!navigation.classList.contains("nav-active")) {
+      subMenus.forEach((subMenu) => {
+        subMenu.classList.remove("sub-menu-active");
+      });
+      navArrows.forEach((arrow) => {
+        arrow.classList.remove("rotate-arrow");
+      });
+    }
+  });
+
+  // open the sub-menu
+  navLinks.addEventListener("click", (e) => {
+    // stop the event on desktop version
+    if (navIsActive) {
+      if (e.target.parentElement.children[1]) {
+        e.target.parentElement.children[1].classList.toggle("sub-menu-active");
+        e.target.children[0].classList.toggle("rotate-arrow");
+      }
+    }
+  });
+
+  //close other sub menu
+};
+
+const app = () => {
+  activateNav();
+};
+
+app();
+
+//FIXME  2) btn-group
